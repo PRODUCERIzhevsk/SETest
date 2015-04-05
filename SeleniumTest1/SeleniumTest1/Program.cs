bbe4c77890d03f8c98a280f7ba54bab30de757dd
ehd1996@mail.ru <ehd1996@mail.ru>
2015-04-05 20:19:33 +0400

    net test project

---
+++ SeleniumTest1/SeleniumTest1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.IE;
+using OpenQA.Selenium.Chrome;
+
+namespace SeleniumTest1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("http://www.plexusdev.ru/setest");
+            Thread.Sleep(3000);
+
+            driver.Manage().Window.Position = new Point(0, 0);
+            driver.Manage().Window.Size = new Size(640, 360);
+            var btn = driver.FindElement(By.Id("btnPhotos"));
+            btn.Click();
+            Thread.Sleep(3000);
+
+            var screenShot1 = (driver as ITakesScreenshot).GetScreenshot();
+            screenShot1.SaveAsFile("screen1.png", ImageFormat.Png);
+
+            //Console.ReadKey();
+            driver.Manage().Window.Size = new Size(1920, 1080);
+            Thread.Sleep(3000);
+            var screenShot2 = (driver as ITakesScreenshot).GetScreenshot();
+            screenShot2.SaveAsFile("screen2.png", ImageFormat.Png);
+
+            driver.Close();
+        }
+    }
+}
